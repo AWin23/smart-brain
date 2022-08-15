12faef237977c44aa2da88f50da6f7496d4904bc
Andrew Nguyen <nguyendrew20@gmail.com>
2022-08-15 00:37:26 -0700

    Reloaded the tilt

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import Tilt from 'react-tilt';
+import Tilt from 'react-tilt';
 import brain from './brain.png';
 import './Logo.css';
 
@@ -7,8 +7,9 @@ import './Logo.css';
 const Logo = () => {
 	return (
 		<div className='ml4 mt0' style={{ height: 150, width: 150 }}>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 130, width: 150 }} >
              <div className="Tilt-inner"><img alt='logo' src={brain}/></div>
-		
+			</Tilt>
 		</div>
 	);
 }
